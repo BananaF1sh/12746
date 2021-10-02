4585413380b4feea1a9371a0bd97471845e0417d
BananaF1sh <yipeng@andrew.cmu.edu>
2021-10-02 15:14:34 -0400

    add Pittsburgh map

--- Course_project/main.py
+++ Course_project/main.py
@@ -1,5 +1,14 @@
 import geopandas as gpd
 import pandas as pd
+import matplotlib.pyplot as plt
 
 bridges = pd.read_csv("Course_project\PittsburghBridges.csv")
-print(bridges)
+bridge_geo = gpd.GeoDataFrame(bridges)
+
+map = gpd.GeoDataFrame.from_file("Course_project\PittsburghBridges.csv")
+print(map)
+
+points = gpd.GeoDataFrame(bridges,geometry=gpd.points_from_xy(bridges.longitude, bridges.latitude))
+print(points.head())
+points.plot()
+plt.show()
